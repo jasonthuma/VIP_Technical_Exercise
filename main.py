f956b411f10464b0d79de8382247b13631626b1b
Jason <jasonthuma@gmail.com>
2020-12-19 18:40:26 -0500

    findUniquePairs implemented

--- main.py
+++ main.py
@@ -29,11 +29,29 @@ def findUniquelyOrderedPairs(paramList, targetSum):
             print()
 
 
+def findUniquePairs(paramList, targetSum):
+    uniqueOutputPairs = []
+    for i in range(len(paramList)):
+        for j in range(len(paramList)):
+            if i != j and paramList[i] + paramList[j] == targetSum:
+                matchingIndices = (paramList[i], paramList[j])
+                reversedMatchingIndices = (paramList[j], paramList[i])
+                if matchingIndices not in uniqueOutputPairs and reversedMatchingIndices not in uniqueOutputPairs:
+                    uniqueOutputPairs.append(matchingIndices)
+    for i in range(len(uniqueOutputPairs)):
+        print(uniqueOutputPairs[i], end="")
+        if i < len(uniqueOutputPairs) - 1:
+            print(", ", end="")
+        else:
+            print()
+
+
 def main():
     argList = [1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9]
     desiredSum = 10
     findAllPairs(argList, desiredSum)
     findUniquelyOrderedPairs(argList, desiredSum)
+    findUniquePairs(argList, desiredSum)
 
 
 main()
